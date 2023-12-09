31694f1b816455b042b61fcca8abde0b3f31facb
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-09 13:13:04 -0600

    created GET and POST on server.js. Also created calculateResults function.

--- README.md
+++ README.md
@@ -6,4 +6,17 @@
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
+ - TODO 
+ Client side
+    - create form in html to enter two values and buttons for different math operators
+        -  will need to store which operator was choosen
+    - need a button to calculate and use that to run functions to send data to server
+    - create section to display history of operations
+
+
+ Server side
+    - create functions to complete math operations on SERVER side
+    - store 'history' of calculations made in a server side array
+    - express functions for POST and GET
+
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser')
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}))
 
 // Global variable that will contain all of the
 // calculation objects:
@@ -13,10 +15,41 @@ let calculations = []
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
+app.get('/calculations', (req, res)=>{
+  console.log('In GET /calculations', calculations)
+  res.send(calculations)
+})
 
 // POST /calculations
+app.post('/calculations', (req, res)=>{
+  console.log('In POST /calculations', req.body)
+  calculateResult(req.body)
+  console.log(req.body)
+  res.sendStatus(201);
+})
 
 
+function calculateResult(incObject){
+  let number1 = JSON.parse(incObject.num1)
+  let number2 = JSON.parse(incObject.num2)
+
+  if (incObject.operator==='+'){
+    incObject.result = number1 + number2
+  }
+  if (incObject.operator==='-'){
+    incObject.result = number1 - number2
+  }
+  if (incObject.operator==='*'){
+    incObject.result = number1 * number2
+  }
+  if (incObject.operator==='/'){
+    incObject.result = number1 / number2
+  }
+
+}
+
+
+// ! DON'T GO BELOW
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
